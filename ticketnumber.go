92c3b027fe9e4a2ee6d55d3b40d98b1940a007ba
xaoxong <xaoxong@gmail.com>
2018-11-13 12:10:49 +0800

    如果SSR TKNE获取票号失败从TN项获取

--- ticketnumber.go
+++ ticketnumber.go
@@ -7,6 +7,7 @@ import (
 
 type TicketNumberLine struct {
 	TicketNumberList []*TicketNumber
+	isTn             bool
 }
 
 func NewTktLine() *TicketNumberLine {
@@ -14,7 +15,14 @@ func NewTktLine() *TicketNumberLine {
 }
 
 func (t *TicketNumberLine) IsMatch(line string) bool {
-	return strings.HasPrefix(line, "SSR TKNE")
+	if strings.HasPrefix(line, "SSR TKNE") {
+		return true
+	}
+	if strings.HasPrefix(line, "TN/") {
+		t.isTn = true
+		return true
+	}
+	return false
 }
 
 func (t *TicketNumberLine) Data() []*TicketNumber {
@@ -25,12 +33,24 @@ func (t *TicketNumberLine) Add(pos int, line string, pl *PersonLine) bool {
 	if t.IsMatch(line) == false {
 		return false
 	}
-	tktItmes := strings.Fields(line)
+
 	tkt := &TicketNumber{}
-	// SSR TKNE CA HK1 PEKMEL 165 W26SEP 9992876664435/1/P2
-	tkt.Airline = tktItmes[2]
-	rph := strings.Split(tktItmes[7], "/")
-	tkt.Number = rph[0]
+	//rph := ""
+
+	// TN/000-000000000/P1
+	if t.isTn {
+		itemAry := strings.Split(line, "/")
+		tkt.Number = itemAry[1]
+		tkt.PersonRPH = t.rphToi(itemAry[2]) //strconv.Atoi(strings.Replace(itemAry[2], "P", "", -1))
+	} else {
+		tktItmes := strings.Fields(line)
+		// SSR TKNE CA HK1 PEKMEL 165 W26SEP 9992876664435/1/P2
+		tkt.Airline = tktItmes[2]
+		rph := strings.Split(tktItmes[7], "/")
+		tkt.Number = rph[0]
+		tkt.JourneyRPH, _ = strconv.Atoi(rph[1])
+		tkt.PersonRPH = t.rphToi(rph[2]) //strconv.Atoi(strings.Replace(rph[2], "P", "", -1))
+	}
 
 	for _, v := range t.TicketNumberList {
 		if v.Number == tkt.Number {
@@ -38,12 +58,16 @@ func (t *TicketNumberLine) Add(pos int, line string, pl *PersonLine) bool {
 		}
 	}
 
-	tkt.JourneyRPH, _ = strconv.Atoi(rph[1])
-	tkt.PersonRPH, _ = strconv.Atoi(strings.Replace(rph[2], "P", "", -1))
 	pl.SetTktNumber(tkt.PersonRPH, tkt.Number)
 	t.TicketNumberList = append(t.TicketNumberList, tkt)
 	//fmt.Println(tkt.Number)
 	return true
+}
+
+func (t *TicketNumberLine) rphToi(rph string) int {
+	rph = strings.TrimSpace(rph)
+	rev, _ := strconv.Atoi(strings.Replace(rph, "P", "", -1))
+	return rev
 }
 
 type TicketNumber struct {
